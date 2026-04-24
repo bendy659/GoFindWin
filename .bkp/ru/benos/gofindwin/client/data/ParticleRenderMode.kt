0cde3945ba59a739b64b32dea1333ce79802ad06
_BENDY659_ <109421782+bendy659@users.noreply.github.com>
2026-04-24 18:20:23 +0900

    Cleanup. Removing AI code. Refactoring (Recoding) Ui lib (AutoUiLayout -> UiLayout)

--- .bkp/ru/benos/gofindwin/client/data/ParticleRenderMode.kt
+++
@@ -1,9 +0,0 @@
-package ru.benos.gofindwin.client.data
-
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
-
-@Environment(EnvType.CLIENT)
-enum class ParticleRenderMode(val id: String) {
-    FLAT("flat"), VOLUME("volume")
-}
